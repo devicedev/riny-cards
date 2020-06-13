26c0e96a435370e5ca34a47c3ee7867d0b0ec5ae
Hutu Tudor <hutu.tudor@yahoo.com>
2020-06-13 21:54:05 +0300

    Epic refactoing

---
+++ src/components/DecksMenu.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MenuItem } from ".";
+
+export const DecksMenu = () => {
+  const [items] = useState(initialState);
+
+  return (
+    <Wrapper>
+      {items.map((item) => (
+        <MenuItem key={item.name} item={item} />
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1px solid #c5c5c5;
+`;
+
+const initialState = [
+  {
+    name: "Decks",
+    active: true,
+  },
+  {
+    name: "Followers",
+    active: false,
+  },
+  {
+    name: "Following",
+    active: false,
+  },
+];
